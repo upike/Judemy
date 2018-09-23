09b5af41d0c34168d40ab2b40d8abeebe3c7934b
upike <javadkpobieranie@gmail.com>
2018-09-23 21:52:46 +0200

    Moj druki commit MJS

--- src/pl/szczupakmariusz/udemy/firstproject/Main.java
+++ src/pl/szczupakmariusz/udemy/firstproject/Main.java
@@ -3,5 +3,12 @@ package pl.szczupakmariusz.udemy.firstproject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello IntelliJ!");
+        System.out.println("Wprowadzam zmiany które będą za commitowane i wypushowane na server github");
+        // to jest komentarz jedno liniowy
+        /*to jest komentarz wielo liniowy
+        fdafadsfasdfasdf
+        na zielono zaznaczone są zmiany w pliku ponieważ jest wrlaczona kontrola wersji
+        wciskamy Ctr + K to zrobi committa
+         */
     }
 }
